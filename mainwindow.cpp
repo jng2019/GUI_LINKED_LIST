292e6d2201348df5588a83b2a4f6ff7e6231f0f6
Jonathan Ng <jonathanng1016@gmail.com>
2022-04-19 09:35:35 -0700

    Still need to fix adding linked lists to vector

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,7 +2,7 @@
 #include "./ui_mainwindow.h"
 #include "vectorList.h"
 
-LinkedList<int> link;
+VectorList<LinkedList<int>> v;
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -16,19 +16,57 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
-void MainWindow::on_pushButton_clicked()
+void MainWindow::on_pushButton_7_clicked() // push front
 {
+    int row = ui->listWidget->currentRow();
     int input;
     input = ui->lineEdit->text().toInt();
-    link.push_back(input);
+    (v.at(row)).push_front(input);
 }
 
-
-void MainWindow::on_pushButton_3_clicked()
+void MainWindow::on_pushButton_clicked() // push back button
 {
-    QString output;
-    output = QString::fromStdString(link.getString());
-    ui->textBrowser->setText(output);
+    int row = ui->listWidget->currentRow();
+    int input;
+    input = ui->lineEdit->text().toInt();
+    (v.at(row)).push_back(input);
 }
+
+
+void MainWindow::on_pushButton_3_clicked()  //display button
+{
+    int row = ui->listWidget->currentRow();
+    QString output;
+    output = QString::fromStdString(to_string(row));
+//    output = QString::fromStdString(v.at(row-1).getString());
+    if(row==-1)
+    {
+        ui->textBrowser->setText(QString::fromStdString("No list selected!"));
+    }
+    else
+        ui->textBrowser->setText(output);
+}
+
+
+void MainWindow::on_pushButton_2_clicked()  //add list
+{
+    bool intl, doublel, stringl, rationall, datel, complex;
+    intl = ui->radioButton->isChecked();
+    doublel = ui->radioButton_2->isChecked();
+    stringl = ui->radioButton_3->isChecked();
+    rationall = ui->radioButton_4->isChecked();
+    datel = ui->radioButton_5->isChecked();
+    complex = ui->radioButton_6->isChecked();
+
+    if (intl)
+    {
+        ui->listWidget->addItem(QString::fromStdString("IntList"));
+        LinkedList<int> pushint;
+
+        //v.push_back(pushint);
+    }
+}
+
+
+
 
--- vectorList.h
+++ vectorList.h
@@ -8,9 +8,10 @@ class VectorList
     private:
         unsigned cap;   //IN/OUT - capacity of vector
         unsigned sz;    //IN/OUT - size of vector
-        LinkedList<E> *data;      //IN/OUT - points to memory address of dynamically created array
+        E *data;      //IN/OUT - points to memory address of dynamically created array
 
         void expand();
+        void expand(int amount);
     public:
 
         /***************
@@ -30,12 +31,12 @@ class VectorList
         unsigned size( ) const;
         unsigned capacity( ) const;
         bool empty( ) const;
-        const LinkedList<E> & at( unsigned index ) const;
+        E& at( unsigned index ) const;
 
         /***************
         ** MUTATORS **
         ***************/
-        void push_back( LinkedList<E> &list );
+        void push_back( E &list );
         void pop_back( );
         void clear( );
         void resize( unsigned size );
@@ -54,7 +55,21 @@ void VectorList<E>::expand()
         temp.push_back(data[i]);
     }
     delete data;
-    data = new LinkedList<E>[cap];
+    data = new E[cap];
+    data = temp.data;
+}
+
+template <typename E>
+void VectorList<E>::expand(int amount)
+{
+    cap+=amount;
+    VectorList<E> temp;
+    for(int i = 0; i < sz;i++)
+    {
+        temp.push_back(data[i]);
+    }
+    delete data;
+    data = new E[cap];
     data = temp.data;
 }
 
@@ -63,7 +78,7 @@ VectorList<E>::VectorList()
 {
     sz=0;
     cap=0;
-    data = new LinkedList<E>[0];
+    data = new E[0];
 }
 
 template <typename E>
@@ -94,7 +109,7 @@ bool VectorList<E>::empty() const
 }
 
 template <typename E>
-const LinkedList<E>& VectorList<E>::at( unsigned index ) const
+E& VectorList<E>::at( unsigned index ) const
 {
     if (index < sz)
         return data[index];
@@ -104,15 +119,25 @@ const LinkedList<E>& VectorList<E>::at( unsigned index ) const
 ** MUTATORS **
 ***************/
 template <typename E>
-void VectorList<E>::push_back( LinkedList<E> &list )
+void VectorList<E>::push_back( E &list )
 {
     resize(++sz);
     if(sz>cap)
         expand();
    data[sz-1]=list;
 }
-void pop_back( );
-void clear( );
+
+template <typename E>
+void VectorList<E>::pop_back( )
+{
+
+}
+
+template <typename E>
+void VectorList<E>::clear( )
+{
+
+}
 template <typename E>
 void VectorList<E>::resize( unsigned size )
 {
